709e240ef60ef4774a4a27ed46bc26f3976bbef0
Subham1 <sahusubham1975@gmail.com>
2025-02-09 12:55:17 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import List from "./components/List";
 import Detail from "./components/Detail";
 import Navbar from "./components/Navbar";
-import ImageUpload from "./components/Image";
+
 const App = () => {
   return (
     <>
